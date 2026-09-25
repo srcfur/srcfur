6c47b29eb2268b329b3ceea49562b7e9c5d9a01a
Rezzy <74584623+srcfur@users.noreply.github.com>
2026-09-24 23:04:33 -0400

    Changed upload page to redirect to gallery after upload!

---
+++ public/javascripts/upload_backend.js
@@ -0,0 +1,22 @@
+function upload_handler(){
+    const form = document.querySelector("#UploadForm")
+    const submitbutton = form.querySelector("#SubmitForm");
+
+    submitbutton.addEventListener("click", function(e) {
+        e.preventDefault();
+        let data = new FormData(form);
+        fetch("/api/gallery/upload", { method: "POST", body: data }).then(async response => {
+            if(!response.ok){
+                window.alert(response.status + ": " + await response.text());
+                return;
+            }
+            window.location.assign("/gallery");
+        })
+    })
+}
+
+if(document.readyState == "loading"){
+    document.addEventListener("DOMContentLoaded", upload_handler)
+}else{
+    upload_handler();
+}
