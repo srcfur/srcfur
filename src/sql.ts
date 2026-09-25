6e5477a7bb97baad826c6408c22b25a1e2a05e4f
Rezzy <74584623+srcfur@users.noreply.github.com>
2026-09-24 22:40:19 -0400

    Secure sql :3

--- src/sql.ts
+++ src/sql.ts
@@ -32,9 +32,8 @@ fs.readdir("sqlsetup/", (err, files) => {
 export function CreateGalleryPost(PostName:string, PostDescription:string){
     let isoString = new Date(Date.now()).toISOString();
     let query =
-        database.prepare("INSERT INTO gallery_posts(post_date_iso, post_name, post_description) VALUES ('" + isoString + "', '" + PostName + "', '" + PostDescription + "') " +
-            "RETURNING *;");
-    return query.get() as GalleryPostData;
+        database.prepare("INSERT INTO gallery_posts(post_date_iso, post_name, post_description) VALUES (?, ?, ?) RETURNING *;");
+    return query.get(isoString, PostName, PostDescription) as GalleryPostData;
 }
 
 export function AppendImageToGalleryPostData(Post: GalleryPostData, ImagePath: string){
@@ -42,15 +41,14 @@ export function AppendImageToGalleryPostData(Post: GalleryPostData, ImagePath: s
 }
 
 export function AppendImageToGalleryPost(PostID: number, ImagePath:string){
-    let query =
-        database.prepare("INSERT INTO gallery_image(post_id, image_path) VALUES (" + PostID.toString() + ", '" + ImagePath + "');");
-    query.run();
+    let query = database.prepare("INSERT INTO gallery_image(post_id, image_path) VALUES (?, ?)");
+    query.run(PostID.toString(), ImagePath);
 }
 
 export function GetGalleryPage(page:number, pagesize:number): GalleryPostData[] {
     let query =
-        database.prepare("SELECT * FROM gallery_posts ORDER BY id DESC LIMIT " + pagesize + " OFFSET " + (page * pagesize).toString() + ";");
-    return query.all() as GalleryPostData[];
+        database.prepare("SELECT * FROM gallery_posts ORDER BY id DESC LIMIT ? OFFSET ?;");
+    return query.all(pagesize, (page * pagesize)) as GalleryPostData[];
 }
 
 export function GetGalleryPostCount(){
@@ -59,27 +57,27 @@ export function GetGalleryPostCount(){
 
 export function GetImagesFromGalleryPost(postId:number){
     let query =
-        database.prepare("SELECT `image_path` FROM gallery_image WHERE post_id = " + postId + ";");
-    return query.all() as string[];
+        database.prepare("SELECT `image_path` FROM gallery_image WHERE post_id = ?;");
+    return query.all(postId) as string[];
 }
 
 export function CreateCommentOnPost(postId: number, comment: string, authorname: string, authoravatar: string, identifier: string) {
     let query =
         database.prepare("INSERT INTO gallery_comment(comment_content, author_avatar, author_name, author_identifier, post_id) " +
-            " VALUES ('" + comment + "', '" + authoravatar + "', '" + authorname + "', '" + identifier + "', '" + postId + "') ");
-    query.run();
+            " VALUES (?, ?, ?, ?, ?) ");
+    query.run(comment, authoravatar, authorname, identifier, postId);
 }
 
 //Can be scrubbed of it's identifier to hide IP addresses!!!
 export function GetCommentsOnPost(postId: number, scrub:boolean){
     let query =
-        database.prepare("SELECT * FROM gallery_comment WHERE post_id = " + postId + " ORDER BY comment_id DESC");
-    let raw = query.all() as Comment[];
+        database.prepare("SELECT * FROM gallery_comment WHERE post_id = ? ORDER BY comment_id DESC");
+    let raw = query.all(postId) as Comment[];
     if(scrub)
         raw.forEach(comment => { comment.author_identifier = ""; })
     return raw;
 }
 
 export function IsIdentifierBanned(id:string){
-    return (database.prepare("SELECT COUNT(*) FROM banned_identifiers WHERE identifier = '" + id + "'").get() as any)["COUNT(*)"] as number > 0;
+    return (database.prepare("SELECT COUNT(*) FROM banned_identifiers WHERE identifier = ?").get(id) as any)["COUNT(*)"] as number > 0;
 }
